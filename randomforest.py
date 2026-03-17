955e2a4023d8b5acb976c226d29457e4f5844751
khanhhungvl39 <khanhhungvl39@gmail.com>
2026-03-17 17:21:56 +0700

    Initial commit

---
+++ randomforest.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
